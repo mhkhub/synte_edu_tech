21bd0830bbb38a303d902b04553d61f7b78fe7a1
MK <khan.mail.com@gmail.com>
2025-08-15 04:52:51 +0000

    Initial Version

---
+++ synte_edu_tech/synte_mobile_app_for_education/doctype/circulars/circulars.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Synte Solution and Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Circulars", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ synte_edu_tech/synte_mobile_app_for_education/doctype/circulars/test_circulars.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Synte Solution and Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCirculars(FrappeTestCase):
+	pass
